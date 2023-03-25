ecf29e98e672288778e5febd8e1798264e99f863
Butter on Bread <qorbaxk97@gmail.com>
2023-03-25 14:57:36 +0900

    [:lipstick:] 네비바 마이페이지 아이콘 프로필사진으로 등록 #15

--- withpet/src/components/Navigation/Navigation.tsx
+++ withpet/src/components/Navigation/Navigation.tsx
@@ -1,6 +1,17 @@
-import React, { FC, useMemo, useState } from 'react'
+import React, { FC, useMemo, useState, useEffect } from 'react'
 import { Link } from 'react-router-dom'
 import logoSprite from 'assets/sprites_icon.png'
+import navBack from 'assets/navBack.png'
+import { RootState } from 'redux/store'
+import { useSelector } from 'react-redux'
+import {
+  collection,
+  getDocs,
+  DocumentData,
+  query,
+  where,
+} from 'firebase/firestore'
+import { dbService } from 'firebase-config'
 
 type NavigationProps = {
   title?: string
@@ -16,6 +27,9 @@ const NAV_ITEMS = [
 
 const Navigation: FC<NavigationProps> = ({ title = '' }) => {
   const [activeNav, setActiveNav] = useState(title)
+  const [myPets, setMyPets] = useState<DocumentData[]>([])
+
+  const userUid = useSelector((state: RootState) => state.auth.userUid)
 
   const handleClick = (name: string) => {
     setActiveNav(name)
@@ -34,23 +48,71 @@ const Navigation: FC<NavigationProps> = ({ title = '' }) => {
     [],
   )
 
+  useEffect(() => {
+    const getMyPet = async () => {
+      const q = query(
+        collection(dbService, 'petInfo'),
+        where('user', '==', userUid),
+      )
+      const myPetList = await getDocs(q)
+      setMyPets(myPetList.docs.map(doc => doc.data()))
+    }
+    getMyPet()
+  }, [])
+
+  const anotherItems = navItems.filter((e, i) => i <= 3)
+
   return (
     <nav className="w-full max-w-scr h-16 bg-white mx-auto fixed bottom-0 left-0 right-0  z-50">
       <div className="flex flex-nowrap flex-row justify-between leading-8 px-6 py-4 border-t border-solid border-gray-400">
-        {navItems.map(({ name, style }) => (
-          <Link to={`/${name}`} key={name}>
+        {userUid && myPets
+          ? anotherItems.map(({ name, style }) => (
+              <Link to={`/${name}`} key={name}>
+                <button
+                  type="button"
+                  className="w-8.9 h-8.9"
+                  style={
+                    activeNav === name
+                      ? { ...style }
+                      : { ...style, backgroundPositionY: '-45px' }
+                  }
+                  onClick={() => handleClick(name)}
+                  aria-label={`${name} 버튼 선택`}
+                />
+              </Link>
+            ))
+          : navItems.map(({ name, style }) => (
+              <Link to={`/${name}`} key={name}>
+                <button
+                  type="button"
+                  className="w-8.9 h-8.9"
+                  style={
+                    activeNav === name
+                      ? { ...style }
+                      : { ...style, backgroundPositionY: '-45px' }
+                  }
+                  onClick={() => handleClick(name)}
+                  aria-label={`${name} 버튼 선택`}
+                />
+              </Link>
+            ))}
+        {userUid && myPets ? (
+          <Link to={'/mypage'} key={'mypage'}>
             <button
-              className="w-8.9 h-8.9"
-              style={
-                activeNav === name
-                  ? { ...style }
-                  : { ...style, backgroundPositionY: '-45px' }
-              }
-              onClick={() => handleClick(name)}
-              aria-label={`${name} 버튼 선택`}
-            />
+              type="button"
+              onClick={() => handleClick('mypage')}
+              aria-label={'mypage 버튼 선택'}
+            >
+              <img
+                className="w-8.9 h-8.9 rounded-[50%] border-2 border-black border-solid"
+                src={myPets[0] ? myPets[0].petImg : navBack}
+                alt="나의 프로필 사진"
+              />
+            </button>
           </Link>
-        ))}
+        ) : (
+          ''
+        )}
       </div>
     </nav>
   )
