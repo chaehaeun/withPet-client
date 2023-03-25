ccaf63be26c849302e37545a2d5530914e029705
Butter on Bread <qorbaxk97@gmail.com>
2023-03-25 11:22:17 +0900

    [:sparkles:] 현재 위치의 행정구역 표시 #30

--- withpet/src/components/WalkIndex/WalkLocation.tsx
+++ withpet/src/components/WalkIndex/WalkLocation.tsx
@@ -1,15 +1,65 @@
+import spritesIcon from 'assets/sprites_icon.png'
 import React, { useState, useEffect } from 'react'
-import { useDispatch, useSelector } from 'react-redux'
+import { useSelector, useDispatch } from 'react-redux'
+import { getWalkLoading } from 'redux/slice/walkIndex/walkIndexSlice'
 import { RootState } from 'redux/store'
+
 const WalkLocation = () => {
+  const [current, setCurrent] = useState<string>('')
   const loca = useSelector(
     (walkState: RootState) => walkState.walk.walkLocation,
   )
+  const loading = useSelector(
+    (walkState: RootState) => walkState.walk.walkLoading,
+  )
+
+  const dispatch = useDispatch()
+  const currentLocation = async (lat: string, lon: string) => {
+    try {
+      const url = new URL(
+        `https://dapi.kakao.com/v2/local/geo/coord2address.json?x=${lon}&y=${lat}&input_coord=WGS84`,
+      )
+      dispatch(getWalkLoading(true))
+
+      const response = await fetch(url, {
+        headers: {
+          Authorization: `KakaoAK ${process.env.REACT_APP_REST_API}`,
+        },
+      })
+      const data = await response.json()
+      const reg1 = data.documents[0].address.region_1depth_name
+      const reg2 = data.documents[0].address.region_2depth_name
+      const reg3 = data.documents[0].address.region_3depth_name
+      setCurrent(`${reg1} ${reg2} ${reg3}`)
+      dispatch(getWalkLoading(false))
+    } catch (err: any) {
+      console.error(err.message)
+    }
+  }
+
+  useEffect(() => {
+    currentLocation(loca.lat, loca.lng)
+  }, [])
 
   return (
-    <div>
-      <p className="mb-20">{`좌표 ${loca.lat},${loca.lng}`}</p>
-    </div>
+    <>
+      {loading ? (
+        ''
+      ) : (
+        <div className="flex justify-center items-center mb-20">
+          <div
+            style={{
+              backgroundImage: `url(${spritesIcon})`,
+              backgroundPosition: '-79px -402px',
+              width: '12px',
+              height: '16px',
+              marginRight: '16px',
+            }}
+          ></div>
+          <p className="text-white text-xl font-medium">{current}</p>
+        </div>
+      )}
+    </>
   )
 }
 
