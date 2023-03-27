3bb3ea296c17b628db966ff32863a040954cfbf6
chaehaeun <chae_3210@naver.com>
2023-03-27 15:13:01 +0900

    [🔨] 조건에 맞는 댓글 데이터만 불러오게 리팩토링

--- withpet/src/components/Story/CommentWrap.tsx
+++ withpet/src/components/Story/CommentWrap.tsx
@@ -1,8 +1,8 @@
-import React, { useEffect, useState } from 'react'
+import React, { useEffect, useMemo, useState } from 'react'
 import Comment from 'components/Story/Comment'
 import WriteComment from 'components/Story/WriteComment'
 import { dbService } from 'firebase-config'
-import { collection, getDocs } from 'firebase/firestore'
+import { collection, getDocs, query, where } from 'firebase/firestore'
 import { CommentData } from 'redux/slice/story/storySlice'
 import { useSelector } from 'react-redux'
 import { RootState } from 'redux/store'
@@ -13,20 +13,21 @@ type CommentWrapProps = {
 
 const CommentWrap: React.FC<CommentWrapProps> = ({ id }) => {
   const [commentList, setCommentList] = useState<CommentData[]>([])
-  const commentInfoRef = collection(dbService, 'commentInfo')
+  // const commentInfoRef = collection(dbService, 'commentInfo')
   const userUid = useSelector((state: RootState) => state.auth.userUid)
 
-  useEffect(() => {
+  useMemo(() => {
     const getComment = async () => {
       try {
-        const commentSnap = await getDocs(commentInfoRef)
-        const commentData = commentSnap.docs.map((doc): any => doc.data())
-        const commentResult = commentData.filter(
-          (item: any) => item.DiaryId === id,
+        const commentQuery = query(
+          collection(dbService, 'commentInfo'),
+          where('DiaryId', '==', id),
         )
-        commentResult.sort(
-          (a: CommentData, b: CommentData) => a.createdAt - b.createdAt,
-        )
+        const querySnapshot = await getDocs(commentQuery)
+        const commentResult = querySnapshot.docs.map(doc =>
+          doc.data(),
+        ) as CommentData[]
+        commentResult.sort((a, b) => a.createdAt - b.createdAt)
         setCommentList(commentResult)
       } catch (error) {
         console.error('댓글을 가져올 수 없습니다.')
